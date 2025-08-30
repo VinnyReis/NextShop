ad0d5f6783996f29c90fbf5a655d8d879dab7c00
Vinícius Lima Reis <vinicius43333@gmail.com>
2025-08-30 20:18:45 -0300

    project initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="font-sans flex items-center justify-center">
+      <div className="container m-8">
+        Home
+      </div>
+    </div>
+  );
+};
